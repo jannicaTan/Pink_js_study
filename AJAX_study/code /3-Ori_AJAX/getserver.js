21defe6a0ef966924ffcee573189b9070c3859a3
jannicatan <1569315157@qq.com>
2022-02-04 23:20:41 +0800

    AJAX:post请求

--- AJAX_study/code /3-Ori_AJAX/getserver.js
+++ AJAX_study/code /3-Ori_AJAX/getserver.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-
+// 1.GET
 app.get("/server", (request, response) => {
   // 2022.2.3 找出问题所在:这里要设置为setHeader
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.send("hello ajax");
 });
-
+// 2.POST
+app.post("/server", (request, response) => {
+  // 2022.2.3 找出问题所在:这里要设置为setHeader
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.send("hello ajax POST");
+});
 app.listen(8000, () => {
   console.log("8000 success!");
 });
