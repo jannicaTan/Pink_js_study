7d653d970c50dd02e962a3a69d2f3f70a807daf0
jannicatan <1569315157@qq.com>
2022-02-10 19:45:31 +0800

    ES6-start

--- VUE_study/VUE2/预备知识/代码/code/code1/01.默认导出.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/01.默认导出.js
@@ -1,7 +1,9 @@
 let n1 = 10
 let n2 = 20
 function show() {}
-
+// export default {导出的对象}
+// 目的是挂载使外界能够访问导出的对象
+// 注意：只允许使用唯一的一次export default
 export default {
   n1,
   show
--- VUE_study/VUE2/预备知识/代码/code/code1/02.默认导入.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/02.默认导入.js
@@ -1,3 +1,5 @@
+// import 自定义变量名称 from ‘模块标识符’
+// 注意导入中from的.js
 import m1 from './01.默认导出.js'
 
-console.log(m1)
+console.log(m1)//只会输出允许导出的变量
