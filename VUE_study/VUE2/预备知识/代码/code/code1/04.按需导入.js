877c93d5172728b1568bd49368e5d37919955ffe
jannicatan <1569315157@qq.com>
2022-02-10 19:55:48 +0800

    ES6 modules-finish

--- VUE_study/VUE2/预备知识/代码/code/code1/04.按需导入.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/04.按需导入.js
@@ -1,3 +1,4 @@
+// import {按需导出的对应名称1,名称2....} from 对应js文件
 import info, { s1, s2 as str2, say } from './03.按需导出.js'
 
 console.log(s1)
--- VUE_study/VUE2/预备知识/代码/code/code1/05.直接运行模块中的代码.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/05.直接运行模块中的代码.js
@@ -1,3 +1,4 @@
 for (let i = 0; i < 3; i++) {
   console.log(i)
 }
+// 导入的时候直接import js文件路径 即可
