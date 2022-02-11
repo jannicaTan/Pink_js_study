06e055d0d313d25bbc3780782fcdf4b5ec4e94b4
jannicatan <1569315157@qq.com>
2022-02-11 20:56:23 +0800

    Promise-finish

--- VUE_study/VUE2/预备知识/代码/code/code1/07.使用then-fs读取文件内容.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/07.使用then-fs读取文件内容.js
@@ -3,3 +3,5 @@ import thenFs from 'then-fs'
 thenFs.readFile('./files/1.txt', 'utf8').then((r1) => {console.log(r1)})
 thenFs.readFile('./files/2.txt', 'utf8').then((r2) => {console.log(r2)})
 thenFs.readFile('./files/3.txt', 'utf8').then((r3) => {console.log(r3)})
+
+// 上述代码只能保证读取成功 不能保证读取顺序
--- VUE_study/VUE2/预备知识/代码/code/code1/08.基于Promise异步按顺序读取文件内容.js
+++ VUE_study/VUE2/预备知识/代码/code/code1/08.基于Promise异步按顺序读取文件内容.js
@@ -1,18 +1,34 @@
-import thenFs from 'then-fs'
+import thenFs from "then-fs";
 
 thenFs
-  .readFile('./files/11.txt', 'utf8')
-  .catch((err) => {
-    console.log(err.message)
-  })
+  .readFile("./files/1.txt", "utf8")
   .then((r1) => {
-    console.log(r1)
-    return thenFs.readFile('./files/2.txt', 'utf8')
+    console.log(r1);
+    return thenFs.readFile("./files/2.txt", "utf8");//在第一个then中返回一个新的Promise对象
   })
-  .then((r2) => {
-    console.log(r2)
-    return thenFs.readFile('./files/3.txt', 'utf8')
+  .then((r2) => {//继续调用 .then 为上一个.then的返回值（新的Promise实例）指定成功之后的回调函数
+    console.log(r2);
+    return thenFs.readFile("./files/3.txt", "utf8");
   })
-  .then((r3) => {
+  .then((r3)=>{
     console.log(r3)
   })
+
+  // error:进入Promise后发现错误
+  // catch:直接无法进入Promise，只要报错就不运行了直接跳到catch了,可以将catch提前
+// thenFs
+//   .readFile('./files/11.txt', 'utf8')
+//   .catch((err) => {
+//     console.log(err.message)
+//   })
+//   .then((r1) => {
+//     console.log(r1)
+//     return thenFs.readFile('./files/2.txt', 'utf8')//在第一个then中返回一个新的Promise对象
+//   })
+//   .then((r2) => {//继续调用 .then 为上一个.then的返回值（新的Promise实例）指定成功之后的回调函数
+//     console.log(r2)
+//     return thenFs.readFile('./files/3.txt', 'utf8')
+//   })
+//   .then((r3) => {
+//     console.log(r3)
+//   })
