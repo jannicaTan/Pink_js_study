8bd65efeee20ccdaed9c0464eaffc523c0647264
jannicatan <1569315157@qq.com>
2022-02-16 12:27:51 +0800

    webpack-finish

--- VUE_study/VUE2/day1/code/test-change-rows/src/index.js
+++ VUE_study/VUE2/day1/code/test-change-rows/src/index.js
@@ -2,6 +2,6 @@
 import $ from 'jquery'
 // 2.定义入口函数
 $(function(){
-    $('li:odd').css('background-color','red')
+    $('li:odd').css('background-color','pink')
     $('li:even').css('background-color','blue')
 })
